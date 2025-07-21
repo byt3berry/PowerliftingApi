eff7b233153e252f1e75b9bfbab42648d1cc28ff
byt3berry <0xm.this@gmail.com>
2025-07-21 22:56:54 +0200

    Add signal handling for sigint (stop) and sighup (restart)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use actix_web::dev::{Server, ServerHandle};
 use clap::Parser;
+use log::info;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use tokio::sync::Notify;
 
 use cli::Args;
-use crate::server::start_server;
+use crate::server::{handle_sigint_signal, handle_sighup_signal, start_server};
 
 mod api;
 mod cli;
@@ -15,7 +20,26 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
 
     let args: Args = Args::parse();
+    let exit_flag: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+    let restart_notifier: Arc<Notify> = Arc::new(Notify::new());
 
-    start_server(args.ip, args.port)?
-    .await
+    loop {
+        let server: Server = start_server(&args.ip, args.port)?;
+        let handle: ServerHandle = server.handle();
+
+        handle_sigint_signal(&handle, &exit_flag);
+        handle_sighup_signal(&handle, &restart_notifier);
+
+        server.await?;
+
+        if exit_flag.load(Ordering::SeqCst) {
+            break;
+        } else {
+            restart_notifier.notified().await;
+            continue;
+        }
+    }
+
+    info!("Server exited cleanly");
+    Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,12 +1,20 @@
 use actix_htmx::HtmxMiddleware;
-use actix_web::dev::Server;
+use actix_web::dev::{Server, ServerHandle};
 use actix_web::middleware::{Logger, NormalizePath, TrailingSlash};
+use actix_web::rt::signal::ctrl_c;
+use actix_web::rt::signal::unix::{signal, SignalKind};
 use actix_web::{web, App, HttpResponse, HttpServer};
+use log::{info, warn};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use tokio::sync::Notify;
 
 use crate::api::powerlifters::powerlifters;
 use crate::api::root::root;
 
-pub fn start_server(ip: String, port: u16) -> std::io::Result<Server>{
+pub fn start_server(ip: &str, port: u16) -> std::io::Result<Server>{
+    info!("Starting server on {ip}:{port}");
+
     Ok(
         HttpServer::new(|| {
             App::new()
@@ -23,4 +31,29 @@ pub fn start_server(ip: String, port: u16) -> std::io::Result<Server>{
         .bind((ip, port))?
         .run()
     )
+}
+
+pub fn handle_sigint_signal(handle: &ServerHandle, exit_flag: &Arc<AtomicBool>) {
+    let handler_clone: ServerHandle = handle.clone();
+    let flag_clone: Arc<AtomicBool> = exit_flag.clone();
+
+    tokio::spawn(async move {
+        ctrl_c().await.expect("failed to listen for ctrl-c");
+        warn!("SIGINT received: Shutting down server");
+        flag_clone.store(true, Ordering::SeqCst);
+        handler_clone.stop(true).await;
+    });
+}
+
+pub fn handle_sighup_signal(handle: &ServerHandle, notifier: &Arc<Notify>) {
+    let handler_clone: ServerHandle = handle.clone();
+    let notifier_clone = notifier.clone();
+
+    tokio::spawn(async move {
+        let mut sig = signal(SignalKind::hangup()).unwrap();
+        sig.recv().await;
+        warn!("SIGHUP received: Restarting server");
+        handler_clone.stop(true).await;
+        notifier_clone.notify_one();
+    });
 }
